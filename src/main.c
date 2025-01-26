147f37239921193990209951977498a06dadb405
Metzark <jmetzger03@outlook.com>
2025-01-25 21:28:55 -0600

    Refactored and upgraded to SDL3

--- src/main.c
+++ src/main.c
@@ -2,41 +2,54 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <SDL2/SDL.h>
+#include <math.h>
+#include <SDL3/SDL.h>
 
 #pragma endregion Include
 
-#pragma region Macros
+#pragma region Macro
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
 #pragma endregion Macros
 
-#pragma region Declarations
+#pragma region Declare
 
-struct GUI {
+struct GRFX_GUI {
     SDL_Window *window;
     SDL_Renderer *renderer;
+    int running;
 };
 
-struct GUI create_gui();
+// Initialize SDL Library
+void GRFX_Init();
 
-void init_sdl();
+// Destroy renderer and window and quite SDL
+void GRFX_End(struct GRFX_GUI *gui);
 
-void draw_circle(SDL_Renderer *renderer, int centerX, int centerY, int radius);
+// Create a GUI initialized with a window and renderer
+struct GRFX_GUI GRFX_Create_GUI();
 
-#pragma endregion Declarations
+// Clear the renderer with a color
+void GRFX_Clear_GUI(struct GRFX_GUI *gui);
+
+// Draws a filled in circle with radius r, centered at (c_x, c_y)
+void GRFX_Draw_Circle(SDL_Renderer *renderer, int centerX, int centerY, int radius);
+
+// Distance between (x1,y1) and (x2,y2)
+int MAF_Distance(int x1, int y1, int x2, int y2 );
+
+#pragma endregion Declare
 
 int main(int argc, char *argv[]) {
 
     // Initialize SDL
-    init_sdl();
+    GRFX_Init();
 
     // Create GUI with window and renderer
-    struct GUI gui = create_gui();
+    struct GRFX_GUI gui = GRFX_Create_GUI();
 
-    int running = SDL_TRUE;
     SDL_Event event;
 
     int dragging = 0;
@@ -45,27 +58,29 @@ int main(int argc, char *argv[]) {
     int center_x = WINDOW_WIDTH / 2 - 25;
     int center_y = WINDOW_HEIGHT / 2 - 25;
 
-    while (running) {
+    while (gui.running) {
         // Handle events
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
-                case SDL_QUIT:
-                    running = SDL_FALSE;
+                case SDL_EVENT_QUIT:
+                    gui.running = false;
                     break;
-                case SDL_MOUSEBUTTONDOWN:
+                case SDL_EVENT_MOUSE_BUTTON_DOWN:
                     if (event.button.button == SDL_BUTTON_LEFT) {
-                        dragging = 1;
-                        startX = event.button.x - center_x;
-                        startY = event.button.y - center_y;
+                        if(MAF_Distance(center_x, center_y, event.button.x, event.button.y) < 50) {
+                            dragging = 1;
+                            startX = event.button.x - center_x;
+                            startY = event.button.y - center_y;
+                        }
                     }
                     break;
-                case SDL_MOUSEMOTION:
+                case SDL_EVENT_MOUSE_MOTION:
                     if (dragging) {
                         center_x = event.motion.x - startX;
                         center_y = event.motion.y - startY;
                     }
                     break;
-                case SDL_MOUSEBUTTONUP:
+                case SDL_EVENT_MOUSE_BUTTON_UP:
                     if (event.button.button == SDL_BUTTON_LEFT && dragging) {
                         dragging = 0;
                     }
@@ -75,15 +90,14 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // Clear the renderer with a color
-        SDL_SetRenderDrawColor(gui.renderer, 0, 0, 0, 0);
-        SDL_RenderClear(gui.renderer);
+        // Clear GUI before rendering next frame
+        GRFX_Clear_GUI(&gui);
 
         // Set color
         SDL_SetRenderDrawColor(gui.renderer, 255, 255, 0, 255);
 
         // Draw circle
-        draw_circle(gui.renderer, center_x, center_y, 50);
+        GRFX_Draw_Circle(gui.renderer, center_x, center_y, 50);
 
         // Present the renderer (show rendered content on screen)
         SDL_RenderPresent(gui.renderer);
@@ -92,38 +106,38 @@ int main(int argc, char *argv[]) {
         SDL_Delay(16);
     }
 
-    // Destroy the renderer
-    SDL_DestroyRenderer(gui.renderer);
-
-    // Destroy the window
-    SDL_DestroyWindow(gui.window);
-
-    // Quit SDL
-    SDL_Quit();
+    // End
+    GRFX_End(&gui);    
 
     return 0;
 }
 
-// Initialize SDL Library
-void init_sdl() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+#pragma region GRFX Def
+
+void GRFX_Init() {
+    if (SDL_Init(SDL_INIT_VIDEO) != true) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         exit(1);
     }
 }
 
-// Create a GUI initialized with a window and renderer
-struct GUI create_gui() {
-    struct GUI new_gui;
+void GRFX_End(struct GRFX_GUI *gui){
+    SDL_DestroyRenderer(gui->renderer);
+
+    SDL_DestroyWindow(gui->window);
+
+    SDL_Quit();
+}
+
+struct GRFX_GUI GRFX_Create_GUI() {
+    struct GRFX_GUI new_gui;
 
     // Create an SDL window
     new_gui.window = SDL_CreateWindow(
         "Grafx",           
-        SDL_WINDOWPOS_CENTERED,  
-        SDL_WINDOWPOS_CENTERED,  
-        800,                     
-        600,                     
-        SDL_WINDOW_SHOWN         
+        WINDOW_WIDTH,                     
+        WINDOW_HEIGHT,
+        SDL_WINDOW_RESIZABLE
     );
 
     // If window is null, exit
@@ -135,9 +149,8 @@ struct GUI create_gui() {
 
     // Create an SDL renderer
     new_gui.renderer = SDL_CreateRenderer(
-        new_gui.window,                   
-        -1,                       
-        SDL_RENDERER_ACCELERATED
+        new_gui.window,
+        NULL           
     );
 
     // If renderer is null, exit
@@ -148,20 +161,32 @@ struct GUI create_gui() {
         exit(1);
     }
 
+    // Setting gui as 'running'
+    new_gui.running = true;
+
     return new_gui;
 }
 
-// Draws a filled in circle with radius r, centered at (c_x, c_y)
-void draw_circle(SDL_Renderer *renderer, int c_x, int c_y, int r) {
+void GRFX_Clear_GUI(struct GRFX_GUI *gui) {
+    SDL_SetRenderDrawColor(gui->renderer, 0, 0, 0, 0);
+    SDL_RenderClear(gui->renderer);
+}
+
+void GRFX_Draw_Circle(SDL_Renderer *renderer, int c_x, int c_y, int r) {
     for (int y = -r; y <= r; y++) {
         int dx = (int)sqrt(r * r - y * y);
         for (int x = -dx; x <= dx; x++) {
-            SDL_RenderDrawPoint(renderer, c_x + x, c_y + y);
+            SDL_RenderPoint(renderer, c_x + x, c_y + y);
         }
     }
 }
 
-// Distance between (x1,y1) and (x2,y2)
-int distance(int x1, int y1, int x2, int y2 ) {
-    return (int)sqrt(pow((double)(x2 - x1), 2.0) + pow((double)(y2 - y1), 2.0));
+#pragma endregion GRFX Def
+
+#pragma region MAF Def
+
+int MAF_Distance(int x1, int y1, int x2, int y2 ) {
+    return (int)sqrt((double)(x2 - x1) * (x2 -x1) + (y2 - y1) * (y2 - y1));
 }
+
+#pragma endregion MAF Def
